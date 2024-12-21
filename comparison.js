2bad43fc033ee6cc87bd928f2ea3a49fed0d3b91
hellosiyam <hellosiyambd@gmail.com>
2024-12-22 02:45:01 +0600

    first commit

---
+++ comparison.js
@@ -0,0 +1,15 @@
+// console.log(10>5);
+// console.log(15<10);
+// console.log(10 <= 5);
+// console.log(20 >= 15);
+// console.log(20 == 20);
+// console.log(20 == 10);
+// console.log(20 != 10);
+
+var applePrice = 240;
+var mangoPrice = 160;
+console.log(applePrice>mangoPrice);
+
+
+
+
---
+++ variable.js
@@ -0,0 +1,11 @@
+// Types of Variable
+
+// Number Type
+var orangePrice = 200;
+
+// string Type
+var subject = "CSE";
+
+// boolean
+var passed = true;
+

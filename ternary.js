a3c6970c67d2ce05c2cdd3526d9b283817b0b92a
hellosiyam <hellosiyambd@gmail.com>
2024-12-22 22:13:40 +0600

    complite ternary

---
+++ ternary.js
@@ -0,0 +1,67 @@
+// const age = 20;
+// if (age >= 18) {
+//     console.log("You are eligible for vote");
+    
+// }
+// else{
+//     console.log("You are not eligible for vote");
+    
+// }
+
+// age >=18 ? console.log("you are eligable for vote") : console.log("you are not eligable for vote")
+
+// let price = 5000;
+// const isLeader = false;
+// if (isLeader ===true) {
+//     console.log("get 50% Discount");
+    
+// }
+// else{
+//     price=price+1000;
+//     console.log(price);
+    
+// }
+
+// price = isLeader === true ? console.log("get 50% Discount") : console.log(price=price+1000);
+
+
+// let price = 4000;
+// const isLeader = true;
+// if (isLeader ===true) {
+//     if (price>5000 ) {
+//         price=price/2
+//         console.log(price);
+        
+//     }
+//     else{
+//         console.log(price=0);
+        
+//     }
+    
+// }
+// else{
+//     price=price+1000
+//     console.log(price);
+    
+// }
+
+let price=1200;
+const isLeader = true;
+// if (isLeader === true) {
+//   if (price > 1000) {
+//     price = price / 2
+//     console.log(price);
+    
+//   } 
+//   else{  
+//     console.log(price=0);
+//   } 
+// }
+// else{
+//     console.log(price);
+    
+// }
+
+// price= isLeader===true? console.log(price>1000? price/2 : price=0) : console.log(price+1000);
+price= isLeader===false? console.log(price>1000? price/2 : price=0):console.log(price+1000);
+

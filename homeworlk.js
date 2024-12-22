ffc66e4c9291c3925783765ac95970053c247eae
hellosiyam <hellosiyambd@gmail.com>
2024-12-22 19:52:46 +0600

    complite HomeWork

---
+++ homeworlk.js
@@ -0,0 +1,29 @@
+// const mark = 32;
+// if (mark >= 33) {
+//     console.log("pass With D");
+    
+// }
+// else if (mark >= 40) {
+//     console.log("pass With c");
+    
+// }
+// else if (mark >= 50) {
+//     console.log("pass With B");
+    
+// }
+// else if (mark >= 60) {
+//     console.log("pass With A");
+    
+// }
+// else if (mark >= 70) {
+//     console.log("pass With A+");
+    
+// }
+// else{
+//     console.log("Fail");
+    
+// }
+const grade = 70;
+if (grade ) {
+    
+}
--- stepped.js
+++ stepped.js
@@ -1,18 +1,51 @@
-const price = 3500;
-if(price >= 4000){
-    //10% Discount
-    const discount = price * 10 / 100;
-    const totalPrice = price - discount;
-    console.log(totalPrice);
+// const price = 3500;
+// if(price >= 4000){
+//     //10% Discount
+//     const discount = price * 10 / 100;
+//     const totalPrice = price - discount;
+//     console.log(totalPrice);
+    
+// }
+// else if(price >= 2000){
+//     const discount = price * 5 / 100;
+//     const totalPrice = price - discount;
+//     console.log(totalPrice);
+    
+// }
+// else{
+//     console.log(price);
+    
+// }
+
+const age = 14;
+const price = 5000
+if (age <=12) {
+    console.log("Free food");
     
 }
-else if(price >= 2000){
-    const discount = price * 5 / 100;
-    const totalPrice = price - discount;
-    console.log(totalPrice);
+else if (age <= 18) {
+    const discount = price * 15 / 100;
+    const payment = price - discount;
+    console.log(payment);
     
+    
+}
+else if(age >=60){
+    // 50% Discount
+    const discount = price * 50 / 100;
+    const payment = price - discount;
+    console.log(payment);
+    
+}
+else if (age >= 50) {
+    //20% Discount
+    const discount = price * 20 / 100;
+    const payment = price - discount;
+    console.log(payment);
+     
 }
 else{
     console.log(price);
     
 }
+

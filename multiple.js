c50b269dc96de60e5e167b7376ad2a61ccd9a377
hellosiyam <hellosiyambd@gmail.com>
2024-12-22 17:04:40 +0600

    multiple condition

---
+++ multiple.js
@@ -0,0 +1,12 @@
+const salary = 20000;
+const height = 60;
+const isBCS = true;
+
+if((salary> 25000 && height>55) || isBCS==true){
+    console.log("We are agree");
+    
+}
+else{
+    console.log("We are not agree");
+    
+}
